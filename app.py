97667a734c3daf0b37e60c2460b497e9995cc9bc
kevin arnold flores pacheco <arnold_k2001@hotmail.com>
2024-08-13 19:48:04 -0500

    init api with 3 endpoint

---
+++ app.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# endpoint to save data to db
+@app.get("/save-data")
+def save_data_to_db():
+    #load controller documents
+
+    #split data into chunks
+    
+    #convert data into embeddings
+
+    #store embeddings in db
+    return {"save"}
+
+# endpoint to retrieve data from db
+@app.get("/retrieval")
+def retrieve_data_from_db():
+    #get question to user
+
+    #get retrieve data to db
+
+    #build the prompt
+
+    #ask the LLM
+
+    #get answer from LLM
+    return {"response"}
+
+# endpoint to convert text to speech
+@app.get("/text-to-speech")
+def text_to_speech():
+    return {"speech AUD"}
