63e958b9333f2de5e1f1a463e8daa12d7f974147
kevin arnold flores pacheco <arnold_k2001@hotmail.com>
2024-08-14 15:21:19 -0500

    api: change method endpoints

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 # endpoint to save data to db
-@app.get("/save-data")
+@app.post("/save-data")
 def save_data_to_db():
     #load controller documents
 
@@ -15,7 +15,7 @@ def save_data_to_db():
     return {"save"}
 
 # endpoint to retrieve data from db
-@app.get("/retrieval")
+@app.post("/retrieval")
 def retrieve_data_from_db():
     #get question to user
 
@@ -29,6 +29,6 @@ def retrieve_data_from_db():
     return {"response"}
 
 # endpoint to convert text to speech
-@app.get("/text-to-speech")
+@app.post("/text-to-speech")
 def text_to_speech():
     return {"speech AUD"}
